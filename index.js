df715ef2ec39373f7215423a02758c12ff750f4f
poppy-allen <poppyallen.dev@gmail.com>
2022-01-31 23:32:08 +0000

    Improved JavaScript and added Search Form to HTML
    Added missing day from the array. Added console to update weather icons. Added Search Form in HTML.

--- index.js
+++ index.js
@@ -6,28 +6,30 @@ function displayDate(timestamp) {
         "Tuesday", 
         "Wednesday", 
         "Thursday", 
-        "Friday"
+        "Friday",
+        "Saturday"
     ];
     let day = days[date.getDay()];
     let hours = date.getHours();
     if (hours < 10) {
-        hours = `0${hours}`
+        hours = `0${hours}`;
     }
     let minutes = date.getMinutes();
     if (minutes < 10) {
-        minutes = `0${minutes}`
+        minutes = `0${minutes}`;
     }
     return `${day} ${hours}:${minutes}`; 
     }
-    
 
 function displayTemperature(response) {
+    console.log(response.data);
     let temperature = document.querySelector("#temp");
     let location = document.querySelector("#city");
     let weatherCondition = document.querySelector("#condition");
     let humidityPercent = document.querySelector("#humidity");
     let windSpeed = document.querySelector("#wind");
     let dateTime = document.querySelector("#date");
+    let weatherIcon = document.querySelector("#icon");
     temperature.innerHTML = Math.round (response.data.main.temp);
     location.innerHTML = response.data.name;
     weatherCondition.innerHTML = response.data.weather[0].main;
@@ -37,6 +39,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "f25acbf494c6e1996ef769070be0a2e9";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=london,uk&appid=${apiKey}&units=metric`;
+let city = "Paris";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
