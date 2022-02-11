256df1c7d475765e229beda6da05bf6cf0a8814b
poppy-allen <poppyallen.dev@gmail.com>
2022-02-11 14:22:14 +0000

    Added placeholder city and deployed on Netlify

--- index.js
+++ index.js
@@ -125,4 +125,7 @@ function getForecast(coordinates) {
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayForecast);
 }
+
+search("London");
+
 let celsiusTemp = null;
